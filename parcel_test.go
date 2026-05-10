9c88af1a1cce3f821ed7610ad31286a27ef3f4fd
Salim Bersirov <bersirovsalim@gmail.com>
2026-05-10 11:20:18 +0300

    fix review comments

--- parcel_test.go
+++ parcel_test.go
@@ -9,7 +9,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
 var (
 	// randSource источник псевдо случайных чисел.
 	// Для повышения уникальности в качестве seed
